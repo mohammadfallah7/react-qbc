e875862be2b23435fa0734e419c1deb2e6d3fee8
Mohammad_Fallah <mohammadfallah.w@gmail.com>
2025-05-18 20:26:23 +0330

    feat: Install tailwindcss & Your First Component react doc

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,20 @@
+const Profile = () => {
+  return <img src="https://i.imgur.com/MK3eW3Am.jpg" alt="Katherine Johnson" />;
+};
+
+const Gallery = () => {
+  return (
+    <section>
+      <h1>Amazing scientists</h1>
+      <Profile />
+      <Profile />
+      <Profile />
+    </section>
+  );
+};
+
 const App = () => {
-  return <h1>Hello World!</h1>;
+  return <Gallery />;
 };
 
 export default App;
