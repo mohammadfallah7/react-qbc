271ba6f2df410ffa0e5ec21b9c480147809f349e
Mohammad_Fallah <mohammadfallah.w@gmail.com>
2025-05-21 20:54:31 +0330

    feat: JavaScript in JSX with Curly Braces react doc

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,7 +1,18 @@
 const TodoList = () => {
+  const person = {
+    username: "Hedy Lamarr",
+    theme: { color: "red", fontWeight: "bold", fontSize: "3rem" },
+  };
+
+  function formatDate(date) {
+    return new Intl.DateTimeFormat("en", { weekday: "short" }).format(date);
+  }
+
   return (
     <>
-      <h1>Hedy Lamarr's Todos</h1>
+      <h1 style={person.theme}>
+        {person.username}'s Todos for {formatDate(new Date())}
+      </h1>
       <img
         src="https://i.imgur.com/yXOvdOSs.jpg"
         alt="Hedy Lamarr"
