ec16ec4a128efab2fc2b7fe952988a8679592b1e
Mohammad_Fallah <mohammadfallah.w@gmail.com>
2025-06-04 20:14:04 +0330

    feat: update state in object

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,9 @@
-import { useState } from "react";
-
-const users = ["Mohammad", "Reza", "Ahmad", "Ali"];
+import ListGroup from "./components/ListGroup";
 
 const App = () => {
-  const [index, setIndex] = useState(0);
-
-  const user = users[index];
-
   return (
     <div className="container mx-auto mt-5 ">
-      <button
-        disabled={index === 0}
-        onClick={() => setIndex(index - 1)}
-        className="bg-red-300 rounded cursor-pointer px-2 py-1"
-      >
-        Prev
-      </button>
-      <button
-        disabled={index > 2}
-        onClick={() => setIndex(index + 1)}
-        className="bg-blue-300 rounded cursor-pointer px-2 py-1"
-      >
-        Next
-      </button>
-      <h1>{user}</h1>
+      <ListGroup />
     </div>
   );
 };
--- src/components/ListGroup.jsx
+++ src/components/ListGroup.jsx
@@ -1,7 +1,42 @@
-import React from "react";
+import { useState } from "react";
 
 const ListGroup = () => {
-  return <div></div>;
+  const [drink, setDrink] = useState({ title: "Americano", price: 5 });
+  const [user, setUser] = useState({
+    name: "Mohammad",
+    age: 24,
+    email: "mohammad@email.com",
+    nationality: "Iranian",
+  });
+
+  const handleClick = () => {
+    setUser({
+      ...user,
+      nationality: "American",
+    });
+  };
+
+  return (
+    <div className="px-5">
+      <button
+        className="bg-blue-500 px-2 py-1 text-white rounded cursor-pointer"
+        onClick={handleClick}
+      >
+        Update
+      </button>
+
+      <ul className="flex flex-col max-w-lg mx-auto mt-3">
+        <li className="flex justify-between items-center">
+          <h2>{drink.title}</h2>
+          <span>{drink.price}</span>
+        </li>
+        <li className="flex justify-between items-center">
+          <h2>{user.name}</h2>
+          <span>{user.nationality}</span>
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default ListGroup;
